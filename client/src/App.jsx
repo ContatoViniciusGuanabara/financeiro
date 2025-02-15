048f4c7aeb5a0b1c888b4625215d34da82a90615
ContatoViniciusGuanabara <contatoviniciusguanabara@gmail.com>
2025-02-15 05:05:51 -0300

    Criando o Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Singin from './pages/Singin';
+import Singup from './pages/Singup';
+import Profile from './pages/Profile';
 
-export default function App() {
+ export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
-  )
+    <BrowserRouter>
+    {/* header */}
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+		    <Route path="/about" element={<About />} />
+		    <Route path="/sing-in" element={<Singin />} />
+		    <Route path="/sing-up" element={<Singup />} />
+		    <Route path="/singup" element={<Singup />} />
+        </Routes>
+    </BrowserRouter>
+  );
 }
